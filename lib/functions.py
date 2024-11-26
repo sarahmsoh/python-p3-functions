e3d3d9318f874d7aaa4f798c4a184f9f75a7c2cd
sarahmsoh <sarahmuriithi523@gmail.com>
2024-11-26 14:43:16 +0300

    functions update

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
+    print(f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
 def add(num1, num2):
-    pass
+    sum = num1 + num2
+    return sum
 
 def halve(number):
+    if not isinstance(number , (int, float)):
+        return None
+    return number /2
     pass
